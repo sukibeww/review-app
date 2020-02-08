a0d6368d20dc67086a6c03782bd83b386cd62f8a
sukibeww <sukialiong@gmail.com>
2020-02-08 18:56:19 +1100

    initialised project

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Home from './screens/home';
+
+export default function App() {
+  return (
+    <Home/>
+  );
+}
